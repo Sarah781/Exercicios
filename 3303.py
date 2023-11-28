0d7241ebe389e942bf1d15e8010e498339af8fb1
Sarah781 <sarah7bvs@gmail.com>
2023-11-28 10:19:26 -0300

    Wrong answer (35%)

--- 3303.py
+++ 3303.py
@@ -1,5 +1,4 @@
 """ Recentemente Juquinha aprendeu a falar palavrões. Espantada com a descoberta do garoto, sua mãe o proibiu de falar qualquer palavrão, sobre o risco de o menino perder sua mesada.
-
 Como Juquinha odeia ficar sem mesada, ele te contratou para desenvolver um programa que informe para ele se uma palavra é um palavrão ou não.
 
 Palavrões são palavras que contém dez ou mais caracteres, todas as outras palavras são consideradas palavrinhas.
