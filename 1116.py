1083a16beb1f59d9d9219ff23619eb696bbad969
Sarah781 <sarah7bvs@gmail.com>
2023-12-04 17:53:18 -0300

    1116.py

--- 1116.py
+++ 1116.py
@@ -6,10 +6,15 @@
 
 #Saída
 #Para cada caso mostre o resultado da divisão com um dígito após o ponto decimal, ou “divisao impossivel” caso não seja possível efetuar o cálculo
+#:.1f é usado para imprimir um número flutuante com 1 casa decimal.
+#A função map(int, ...) é usada para converter cada string na lista para um inteiro. Finalmente, os valores convertidos são atribuídos às variáveis x e y. A função split() é então usada para dividir a entrada em uma lista de strings com base nos espaços.
 
-x, y = map(int, input().split()) #A função map(int, ...) é usada para converter cada string na lista para um inteiro. Finalmente, os valores convertidos são atribuídos às variáveis x e y. A função split() é então usada para dividir a entrada em uma lista de strings com base nos espaços.
-if y == 0:
-    print('divisao impossivel')
+#Entrada
+
+x=int(input(""))
+y=int(input(""))
+
+if y==0:
+    print("divisao impossivel")
 else:
-    resultado = x / y
-    print(f"{resultado:.1f}") #:.1f é usado para imprimir um número flutuante com 1 casa decimal.
+    print("%.1f" %(x/y))
